0a8c87aa167648bad35121d6166b08dcf20a212c
Sanket <sinhasanket3@gmail.com>
2023-04-10 13:16:09 +0530

    added interceptor and local storage encryption

--- Home-u/src/app/auth/auth-routing.ts
+++ Home-u/src/app/auth/auth-routing.ts
@@ -1,9 +1,12 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 
 
 export const AuthRouting : Routes = [
-    {path:'', component: LoginComponent}
+    {path:'', component: LoginComponent},
+    {path:'register', component: RegisterComponent},
+
 
 ]
--- Home-u/src/app/auth/components/register/register.component.ts
+++ Home-u/src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageEncryptService } from 'src/app/shared/services/local-storage-encrypt.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor( private localStore: LocalStorageEncryptService) { }
 
   ngOnInit(): void {
   }
